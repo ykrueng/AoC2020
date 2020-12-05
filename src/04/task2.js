49afea753859da073effc8eb658edceaf1705963
Yasirah Krueng <yasirah.krueng@arundo.com>
2020-12-04 22:55:28 -0600

    refactor day4 task2

--- src/04/task2.js
+++ src/04/task2.js
@@ -10,30 +10,24 @@ exports.getValidPassportCount2 = (inputArr) =>
     0
   );
 
-const validate = (fieldsMap) => {
-  return Object.keys(validationMap).reduce(
-    (isValid, vK) => isValid && validationMap[vK](fieldsMap.get(vK)),
-    true
+const validate = (fieldsMap) =>
+  Object.keys(validationMap).every((vK) =>
+    validationMap[vK](fieldsMap.get(vK))
   );
-};
 
-const validateNumbers = (min, max) => (number) =>
-  parseInt(number) >= min && parseInt(number) <= max;
+const validateNumbers = (min, max) => (v) =>
+  parseInt(v) >= min && parseInt(v) <= max;
 
 const validationMap = {
   byr: validateNumbers(1920, 2002),
   iyr: validateNumbers(2010, 2020),
   eyr: validateNumbers(2020, 2030),
-  hgt: (value) =>
-    value?.endsWith("cm")
-      ? validateNumbers(150, 193)(value)
-      : value?.endsWith("in")
-      ? validateNumbers(59, 76)(value)
-      : false,
-  hcl: (value) => Boolean(value?.match(/^#[0-9a-f]{6}/)),
-  ecl: (value) =>
-    ["amb", "blu", "brn", "gry", "grn", "hzl", "oth"].includes(value),
-  pid: (value) => Boolean(value?.match(/^\d{9}$/)),
+  hgt: (v) =>
+    (v?.endsWith("cm") && validateNumbers(150, 193)(v)) ||
+    (v?.endsWith("in") && validateNumbers(59, 76)(v)),
+  hcl: (v) => Boolean(v?.match(/^#[0-9a-f]{6}/)),
+  ecl: (v) => ["amb", "blu", "brn", "gry", "grn", "hzl", "oth"].includes(v),
+  pid: (v) => Boolean(v?.match(/^\d{9}$/)),
 };
 
 exports.validationMap = validationMap;
